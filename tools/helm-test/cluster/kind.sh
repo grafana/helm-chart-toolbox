7e962c90bc724a24a02ee16396aa493020dc4a00
Pete Wall <pete.wall@grafana.com>
2025-07-01 17:50:09 -0500

    First working'ish drop of helm test scripts
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

---
+++ tools/helm-test/cluster/kind.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+createKindCluster() {
+  local testPlan=$1
+  clusterName=$(yq eval '.name + "-test-cluster"' "${testPlan}")
+  clusterConfig=$(yq eval '.cluster.config // ""' "${testPlan}")
+  clusterConfigFile=$(yq eval '.cluster.configFile // ""' "${testPlan}")
+
+  command=(kind create cluster --name "${clusterName}")
+  if [ -f "${clusterConfig}" ]; then command+=(--config <(echo "${clusterConfig}"))
+  elif [ -f "${clusterConfigFile}" ]; then command+=(--config "${clusterConfigFile}")
+  fi
+  if ! kind get clusters | grep -q "${clusterName}"; then
+    echo "${command[@]}"
+    "${command[@]}"
+  fi
+}
+
+deleteKindCluster() {
+  local testPlan=$1
+  clusterName=$(yq eval '.name + "-test-cluster"' "${testPlan}")
+
+  if ! kind delete cluster --name "${clusterName}"; then
+    # Sometimes it just needs a minute and it'll work the second time.
+    # This has to do with something related to Beyla being installed and its eBPF hooks into the node.
+    sleep 60
+    kind delete cluster --name "${clusterName}"
+  fi
+}

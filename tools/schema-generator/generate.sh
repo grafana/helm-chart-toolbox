af8e6fe0041e48e0be5f95d5bd2974b81d692eed
Pete Wall <pete.wall@grafana.com>
2025-06-30 19:42:57 -0500

    Add docs creator tool
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- tools/schema-generator/generate.sh
+++ tools/schema-generator/generate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-CHART_DIR=$1
 
 usage() {
-  echo "USAGE: $0 <Chart Dir>"
+  echo "USAGE: $0 [--chart <Chart Dir>] [--file values.yaml]"
   echo "Generates a JSON schema for the Helm chart values file."
   echo ""
-  echo "  <Chart Dir> - The path to the Helm chart directory."
+  echo "  --chart <Chart Dir> - The path to the Helm chart directory."
+  echo "  --file  <Values File> - The path to a YAML file."
   echo ""
   echo "The following supplemental files are also supported and will be applied to the schema:"
   echo "  <Chart Dir>/schema-mods/*.jq - JQ commands to modify the schema."
@@ -13,14 +13,59 @@ usage() {
   echo "  <Chart Dir>/schema-mods/definitions/{name}.schema.json - Definitions to be added to the schema."
 }
 
-if [ -z "${CHART_DIR}" ]; then
-  echo "Chart directory not defined!"
+CHART_DIR=""
+VALUES_FILE=""
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -c|-chart|--chart)
+      CHART_DIR="$2"
+      shift
+    ;;
+    -f|-file|--file)
+      VALUES_FILE="$2"
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Unexpected argument: $1" 1>&2
+      usage
+      exit 1
+  esac
+  shift
+done
+
+if [ -z "${CHART_DIR}" ] && [ -z "${VALUES_FILE}" ]; then
+  echo "Either --chart or --file option must be provided." 1>&2
   usage
   exit 1
 fi
 
+if [ -n "${CHART_DIR}" ] && [ -n "${VALUES_FILE}" ]; then
+  echo "Both --chart and --file options are mutually exclusive. Please provide only one." 1>&2
+  usage
+  exit 1
+fi
+
+if [ -n "${VALUES_FILE}" ]; then
+  echo "Generating schema based on file: ${VALUES_FILE}" 1>&2
+  if [ ! -f "${VALUES_FILE}" ]; then
+    echo "${VALUES_FILE} is not a file!" 1>&2
+    usage
+    exit 1
+  fi
+
+  CHART_DIR="/tmp/temp-chart"
+  helm create "${CHART_DIR}" > /dev/null
+  cp "${VALUES_FILE}" "${CHART_DIR}/values.yaml"
+else
+  echo "Generating schema based on Helm chart: ${CHART_DIR}" 1>&2
+fi
+
 if [ ! -d "${CHART_DIR}" ]; then
-  echo "${CHART_DIR} is not a directory!"
+  echo "${CHART_DIR} is not a directory!" 1>&2
   usage
   exit 1
 fi
@@ -28,37 +73,37 @@ fi
 set -eo pipefail  # Exit immediately if a command fails.
 shopt -s nullglob # Required when a chart does not use mod files.
 
-helm schema-gen "${CHART_DIR}/values.yaml" > "${CHART_DIR}/values.schema.generated.json"
+helm schema-gen "${CHART_DIR}/values.yaml" > /tmp/values.schema.generated.json
 
 if [ -d "${CHART_DIR}/schema-mods" ]; then
   if [ -d "${CHART_DIR}/schema-mods/definitions" ]; then
     # Add definitions to the schema.
     for file in "${CHART_DIR}"/schema-mods/definitions/*.schema.json; do
-      echo "Setting definition for ${file}..."
       name=$(basename "$file" .schema.json)
+      echo "Setting $name definition for ${file}..." 1>&2
+      jq --indent 4 --arg name "${name}" 'del(.["$schema"])' "${file}" > "/tmp/${name}.tmp"
       jq --indent 4 \
         --arg name "${name}" \
-        --slurpfile data "$file" \
+        --slurpfile data ""/tmp/${name}.tmp"" \
         '.definitions[$name] = $data[0]' \
-        "${CHART_DIR}/values.schema.generated.json" > "${CHART_DIR}/values.schema.modded.json"
-      mv "${CHART_DIR}/values.schema.modded.json" "${CHART_DIR}/values.schema.generated.json"
+        /tmp/values.schema.generated.json > /tmp/values.schema.modded.json
+      mv /tmp/values.schema.modded.json /tmp/values.schema.generated.json
     done
   fi
 
   # Applying JQ mods...
   for file in "${CHART_DIR}"/schema-mods/*.jq; do
-    echo "Applying JQ mod for ${file}..."
-    jq --indent 4 --from-file "$file" "${CHART_DIR}/values.schema.generated.json" > "${CHART_DIR}/values.schema.modded.json"
-    mv "${CHART_DIR}/values.schema.modded.json" "${CHART_DIR}/values.schema.generated.json"
+    echo "Applying JQ mod for ${file}..." 1>&2
+    jq --indent 4 --from-file "$file" /tmp/values.schema.generated.json > /tmp/values.schema.modded.json
+    mv /tmp/values.schema.modded.json /tmp/values.schema.generated.json
   done
 
   # Applying JSON mods...
   for file in "${CHART_DIR}"/schema-mods/*.json; do
-    echo "Applying JSON mod for ${file}..."
-    jq --indent 4 -s '.[0] * .[1]' "${CHART_DIR}/values.schema.generated.json" "$file" > "${CHART_DIR}/values.schema.modded.json"
-    mv "${CHART_DIR}/values.schema.modded.json" "${CHART_DIR}/values.schema.generated.json"
+    echo "Applying JSON mod for ${file}..." 1>&2
+    jq --indent 4 -s '.[0] * .[1]' /tmp/values.schema.generated.json "$file" > /tmp/values.schema.modded.json
+    mv /tmp/values.schema.modded.json /tmp/values.schema.generated.json
   done
 fi
 
-mv "${CHART_DIR}/values.schema.generated.json" "${CHART_DIR}/values.schema.json"
-echo "Done: ${CHART_DIR}/values.schema.json"
+cat /tmp/values.schema.generated.json

bbb15528345a591829da4a1d780b4400ee249edf
Pete Wall <pete.wall@grafana.com>
2025-07-09 09:18:18 -0500

    Deploy a configmap with the test parameters, which can be used by the subject under test
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- tools/helm-test/includes/cluster/common.sh
+++ tools/helm-test/includes/cluster/common.sh
@@ -17,3 +17,20 @@ getClusterName() {
   fi
   echo "${clusterName}"
 }
+
+getRandomNumber() {
+  local testPlan=$1
+  testDir=$(dirname "$(readlink -f "${testPlan}")")
+  appendRandomNumber=$(yq eval '.cluster.appendRandomNumber // false' "${testPlan}")
+  randomNumber=""
+  if [ "${appendRandomNumber}" = "true" ]; then
+    randomNumberFile="${testDir}/.random"
+    if [ -f "${randomNumberFile}" ]; then
+      randomNumber=$(cat "${randomNumberFile}")
+    else
+      randomNumber=$(shuf -i 100000-999999 -n 1)
+      echo "${randomNumber}" > "${randomNumberFile}"
+    fi
+  fi
+  echo "${randomNumber}"
+}

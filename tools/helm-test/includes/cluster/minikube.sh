effa2c2868b75b011f3df1215622faeb8795dc15
Pete Wall <pete.wall@grafana.com>
2026-03-20 11:44:46 -0500

    Add args and driver to minikube
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- tools/helm-test/includes/cluster/minikube.sh
+++ tools/helm-test/includes/cluster/minikube.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 createMinikubeCluster() {
+  local testPlan=$1
   command=(minikube start)
+
+  driver="$(yq eval -r '.cluster.driver' "${testPlan}")"
+  if [ "${driver}" != "null" ] && [ -n "${driver}" ]; then
+    command+=("--driver=${driver}")
+  fi
+
+  argsString="$(yq eval -r -o=json '.cluster.args | join(" ")' "${testPlan}")"
+  if [ "${argsString}" != "null" ] && [ -n "${argsString}" ]; then
+    IFS=" " read -r -a args <<< "${argsString}"
+    command+=("${args[@]}")
+  fi
+
   if ! minikube status; then
     echo "${command[@]}"
     "${command[@]}"

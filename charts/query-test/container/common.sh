5c60c7dc5b610b4d3ce6073637a2588c4e075a86
Pete Wall <pete.wall@grafana.com>
2026-08-21 15:58:16 -0500

    Further checks for traces and profiles queries
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- charts/query-test/container/common.sh
+++ charts/query-test/container/common.sh
@@ -110,7 +110,7 @@ function logs_query {
 
 function traces_query {
   echo "Running TraceQL query: ${TEMPO_URL}?q=${1}..."
-  if ! result=$(curl -sSk --get -u "${TEMPO_USER}:${TEMPO_PASS}" "${TEMPO_URL}" --data-urlencode "q=${1}" 2>&1); then
+  if ! result=$(curl -sSk --fail-with-body --get -u "${TEMPO_USER}:${TEMPO_PASS}" "${TEMPO_URL}" --data-urlencode "q=${1}" 2>&1); then
     echo "Query failed!"
     echo "Error: ${result}"
     return 1
@@ -132,8 +132,19 @@ function traces_query {
 
 function profiles_query {
     echo "Running profiles query: ${1}..."
-    result=$(profilecli query series --output=json --query="${1}")
-    resultCount=$(echo "${result}" 2>/dev/null | jq --slurp 'length')
+    if ! result=$(profilecli query series --output=json --query="${1}" 2>&1); then
+      echo "Query failed!"
+      echo "Error: ${result}"
+      return 1
+    fi
+
+    resultCount=$(echo "${result}" | jq --slurp 'length')
+    if [ -z "${resultCount}" ]; then
+      echo "Query returned an invalid response"
+      echo "Result: ${result}"
+      return 1
+    fi
+
     if [ "${resultCount}" -eq 0 ]; then
       echo "Query returned no results"
       echo "Result: ${result}"
